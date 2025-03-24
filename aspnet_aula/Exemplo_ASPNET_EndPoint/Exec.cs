508a52ee9773e5a832457b6f81e64bff6daf43de
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2025-03-24 17:00:39 -0400

    arrumado

--- aspnet_aula/Exemplo_ASPNET_EndPoint/Exec.cs
+++ aspnet_aula/Exemplo_ASPNET_EndPoint/Exec.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Exemplo_ASPNET_EndPoint
-{
+// Chamar as bibliotecas do ASP.NET
+// Executar o comando para instalar o pacote: dotnet add package Microsoft.AspNetCore
+// Comando para instalar o Swagger: dotnet add package Swashbuckle.AspNetCore
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+// Chamar as bibliotecas do Entity Framework
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+using Exemplo_ASPNET_EndPoint.database;
+
     public class Exec
     {
-        
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            var app = builder.Build();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
     }
-}
